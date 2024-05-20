7ad6795d1b05a8090e8e09f46d0b03af3622ebc7
maxthebe01 <114063571+maxthebe01@users.noreply.github.com>
2024-05-20 19:07:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ paymentForm.addEventListener("submit", payWithPaystack, false);
 function payWithPaystack(e) {
     e.preventDefault();
     const email = document.getElementById("email-address").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
     
     let handler = PaystackPop.setup({
         key: 'pk_live_792b824977a5b9f329259a56318e449532bf79fd', // Replace with your public key
         email: document.getElementById("email-address").value,
         amount: 80 * 100,
         currency: 'ZAR',
-        ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-        // label: "Optional string that replaces customer email"
+        ref: ''+Math.floor((Math.random() * 1000000000) + 1),
+        first_name: firstName,
+        last_name: lastName,
         onClose: function(){
           alert('Window closed.');
     },
     callback: function(response){
-        let message = 'Payment complete! Reference: ' + response.reference;
+        let message = 'Payment complete!. Book will be sent via email. Thank You! Reference: ' + response.reference;
         alert(message);
     }
   });
